dfac581d990ce91fb0a5e170c6a63bc42f9a04eb
jeanmbiz <jeanmbiz@hotmail.com>
2025-05-07 19:48:06 -0300

    feat: add vehicles and vehicles table row components

---
+++ src/components/vehicles-table-row.tsx
@@ -0,0 +1,23 @@
+import { TableCell, TableRow } from '@/components/ui/table'
+
+export function VehiclesTableRow() {
+  return (
+    <TableRow>
+      <TableCell className="font-mono font-medium text-center border-r-2 border-border px-4 py-2">
+        EAD 7328
+      </TableCell>
+      <TableCell className="font-medium border-r-2 border-border px-4 py-2 text-center">
+        000001
+      </TableCell>
+      <TableCell className="text-muted-foreground border-r-2 border-border px-4 py-2 text-center">
+        Implemento
+      </TableCell>
+      <TableCell className="text-muted-foreground border-r-2 border-border px-4 py-2 text-center">
+        F MAX Select
+      </TableCell>
+      <TableCell className="font-medium text-center">
+        Em manutenção
+      </TableCell>
+    </TableRow>
+  )
+}
---
+++ src/components/vehicles.tsx
@@ -0,0 +1,36 @@
+import {
+  Table,
+  TableBody,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table'
+import { VehiclesTableRow } from './vehicles-table-row'
+
+export function Vehicles() {
+  return (
+    <>
+      <div className="space-y-2.5">
+        <div className="border rounded-2xl">
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead className="w-[140px] border-r-2 border-border px-4 py-2 text-center text-lg font-semibold">Placa</TableHead>
+                <TableHead className="w-[140px] border-r-2 border-border px-4 py-2 text-center text-lg font-semibold">Frota</TableHead>
+                <TableHead className="w-[140px] border-r-2 border-border px-4 py-2 text-center text-lg font-semibold">Tipo</TableHead>
+                <TableHead className="w-[140px] border-r-2 border-border px-4 py-2 text-center text-lg font-semibold">Modelo</TableHead>
+                <TableHead className="w-[140px] text-center text-lg font-semibold">Status</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {Array.from({ length: 20 }).map((_, i) => {
+                return <VehiclesTableRow key={i} />
+              })}
+            </TableBody>
+          </Table>
+        </div>
+        <div>paginação / scroll infinito</div>
+      </div>
+    </>
+  )
+}
