2fe77397102ee02831ec0adb7eca4d44b926ffdc
jeanmbiz <jeanmbiz@hotmail.com>
2025-05-07 19:18:11 -0300

    feat: add light/dark mode component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import { Helmet, HelmetProvider } from '@dr.pogodin/react-helmet'
+import { ThemeProvider } from './components/theme/theme-provider'
 
 export function App() {
   return (
-    <HelmetProvider>
-      <Helmet titleTemplate="%s | Control 361º" />
-    </HelmetProvider>
+    <ThemeProvider storageKey="control361-theme" defaultTheme="dark">
+      <HelmetProvider>
+        <Helmet titleTemplate="%s | Control 361º" />
+      </HelmetProvider>
+    </ThemeProvider>
   )
 }
