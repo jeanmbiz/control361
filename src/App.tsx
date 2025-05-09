f00857860026cbebeefd06a997cf9bfbe1277098
jeanmbiz <jeanmbiz@hotmail.com>
2025-05-08 23:14:18 -0300

    feat: add API and React Query Provider to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import { Helmet, HelmetProvider } from '@dr.pogodin/react-helmet'
-import { ThemeProvider } from './components/theme/theme-provider'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { APIProvider } from '@vis.gl/react-google-maps'
 import { RouterProvider } from 'react-router-dom'
+import { ThemeProvider } from './components/theme/theme-provider'
+import { env } from './env'
+import { queryClient } from './lib/react-query'
 import { router } from './routes'
 
 export function App() {
@@ -8,7 +12,11 @@ export function App() {
     <ThemeProvider storageKey="control361-theme" defaultTheme="dark">
       <HelmetProvider>
         <Helmet titleTemplate="%s | Control 361º" />
-        <RouterProvider router={router} />
+        <APIProvider apiKey={env.VITE_GOOGLE_MAPS_API_KEY}>
+          <QueryClientProvider client={queryClient}>
+            <RouterProvider router={router} />
+          </QueryClientProvider>
+        </APIProvider>
       </HelmetProvider>
     </ThemeProvider>
   )
