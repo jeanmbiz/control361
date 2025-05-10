cf8fdad3848d6c1af4481aa02f05556d25ebd4d7
jeanmbiz <jeanmbiz@hotmail.com>
2025-05-09 21:33:37 -0300

    refactor: VehicleTableTypes component

--- src/components/vehicle-table-filters.tsx
+++ src/components/vehicle-table-filters.tsx
@@ -1,14 +1,23 @@
-import { Button } from '@/components/ui/button'
-import { Input } from '@/components/ui/input'
-
 import { Label } from '@/components/ui/label'
 import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group'
+import { useVehicleStore } from '@/store/vehiclesStore'
 
-export function VehicleTableFilters() {
+export function VehicleTableTypes() {
+  const type = useVehicleStore(state => state.type)
+  const setType = useVehicleStore(state => state.setType)
+
+  const handleChangeType = (value: 'tracked' | 'others') => {
+    setType(value)
+  }
+
   return (
-    <form className="flex h-20 items-center justify-between bvehicle-b-2">
+    <form className="flex h-20 items-center bvehicle-b-2 gap-5 md:gap-52">
       <span className="text-xl font-semibold">Lista</span>
-      <RadioGroup className="flex items-center gap-5" defaultValue="tracked">
+      <RadioGroup
+        className="flex items-center gap-5"
+        value={type}
+        onValueChange={handleChangeType}
+      >
         <div className="flex items-center space-x-2 cursor-pointer">
           <RadioGroupItem
             id="tracked"
@@ -30,7 +39,7 @@ export function VehicleTableFilters() {
           </Label>
         </div>
       </RadioGroup>
-      <div className="flex items-center gap-5">
+      {/* <div className="flex items-center gap-5">
         <Input
           placeholder="Buscar por placa ou frota"
           className="h-8 w-[320px]"
@@ -43,7 +52,7 @@ export function VehicleTableFilters() {
         >
           Novo
         </Button>
-      </div>
+      </div> */}
     </form>
   )
 }
