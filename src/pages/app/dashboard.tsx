c71e1ca27516dfc7d31b2eebbb3808c29d38444a
jeanmbiz <jeanmbiz@hotmail.com>
2025-05-09 21:31:33 -0300

    refactor: component VehicleTableTypes name

--- src/pages/app/dashboard.tsx
+++ src/pages/app/dashboard.tsx
@@ -1,15 +1,15 @@
 import { Helmet } from '@dr.pogodin/react-helmet'
 import { MapCard } from '../../components/map-card'
-import { VehicleTableFilters } from '../../components/vehicle-table-filters'
-import { Vehicles } from '../../components/vehicles'
+
+import { VehicleTableTypes } from '@/components/vehicle-table-filters'
+import { Vehicles } from '@/components/vehicles'
 
 export function Dashboard() {
   return (
     <>
       <Helmet title="Dashboard" />
       <div className="flex flex-col gap-4">
-        <VehicleTableFilters />
-
+        <VehicleTableTypes />
         <MapCard />
         <Vehicles />
       </div>
