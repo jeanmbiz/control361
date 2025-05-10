fc37bed74b41bcb9b66f54a0f4784cb2f30f660c
jeanmbiz <jeanmbiz@hotmail.com>
2025-05-09 22:44:19 -0300

    feat: add RefetchInterval and StaleTime to store

--- src/store/vehiclesStore.ts
+++ src/store/vehiclesStore.ts
@@ -2,10 +2,14 @@ import { create } from 'zustand'
 
 export type VehicleState = {
   type: 'tracked' | 'others'
+  mapRefetchInterval: number
+  mapStaleTime: number
   setType: (type: 'tracked' | 'others') => void
 }
 
 export const useVehicleStore = create<VehicleState>(set => ({
   type: 'tracked',
+  mapRefetchInterval: 120000, // 2 minutos
+  mapStaleTime: 110000, // 1:50 minuto
   setType: type => set({ type }),
 }))
