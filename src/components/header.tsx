aff353c44e9c75c9d9a3a98e61bd7ab9241f6cf4
jeanmbiz <jeanmbiz@hotmail.com>
2025-05-11 21:52:47 -0300

    style: ajustar CSS

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,7 +4,7 @@ import { ThemeToggle } from './theme/theme-toggle'
 export function Header() {
   return (
     <header className="border-b-2">
-      <div className="flex h-16 items-center gap-6 px-6">
+      <div className="flex h-16 items-center gap-6 ">
         <nav className="space-x-4 lg:space-x-6">
           <Link
             to="/"
@@ -14,7 +14,6 @@ export function Header() {
             <span className="font-bold">Jean Michel Biz</span>
           </Link>
         </nav>
-
         <div className="ml-auto flex items-center gap-2">
           <ThemeToggle />
         </div>
